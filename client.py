b95822e6c5f7edfbf25b26e125e0ccaaa497f2a1
Meghna Malhotra <mm3886@drexel.edu>
2017-05-25 23:15:28 -0400

    Refactored code to make connection to client easier

--- client.py
+++ client.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
 
-import socket               # Import socket module
+import socket
 
-s = socket.socket()         # Create a socket object
-host = "127.0.0.1"          # IP address of my local network
-port = 12345                # Reserve a port for your service.
+class Client:
+   socket = socket.socket()         # Create a socket object
+   host = "127.0.0.1"               # IP address of my local network
 
-s.connect((host, port))
-print(s.recv(1024))
-s.close                     # Close the socket when done
+   def __init__(self, port):
+      self.port = port
+
+   def connect(self):
+      host = Client.host
+      socket = Client.socket
+      socket.connect((host, self.port))
+      print(socket.recv(1024))
+
+   def closeConnection(self):
+      socket.close                     # Close the socket when done
+      print "The connection to port", self.port, "has been closed"
+
+if __name__ == '__main__':
+   port = 111
+   cl = Client(port)
+   cl.connect()
+   cl.closeConnection()
