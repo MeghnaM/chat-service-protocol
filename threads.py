0fc85b20336c34d0b9dd8b39be7e24442dfeb427
Meghna Malhotra <mm3886@drexel.edu>
2017-05-26 15:05:39 -0400

    Working on a server that handles multiple clients

--- threads.py
+++ threads.py
@@ -6,25 +6,28 @@ import client
 
 # Subclass of the Thread class, that implements a new thread using the threading module
 class myThread (threading.Thread):
+   threadLock = threading.Lock()
 
    # Override of the __init__ method to add additional arguments
-   def __init__(self, threadId, name):
+   def __init__(self, threadId, name, channel):
       threading.Thread.__init__(self)  # Calling the constructor of its parent
       self.threadId = threadId
       self.name = name
+      self.channel = channel
 
    # Override of the run() method to implement what the thread should do when started
    def run(self):
-      print "Starting " + self.name
-      print "Running thread: " + self.name
-      print "Exiting " + self.name
+      print "Starting " + self.name + "..."
+      myThread.threadLock.acquire()       # Get lock to synchronize threads
+      self.channel.connect()              # Establish connection between the client and the server
+      myThread.threadLock.release()       # Free lock to release next thread
 
 # TODO: Exception Handling
 class Threads:
 
    def __init__(self, controlPort, dataPort, adminPort):
       self.controlChannel = client.Client(controlPort)
-      #self.dataChannel = client.Client(dataPort)
+      self.dataChannel = client.Client(dataPort)
       #self.adminChannel = client.Client(adminPort)
 
    # Initiate connection from the client to the server
@@ -32,23 +35,23 @@ class Threads:
       print "Connecting to the control channel on the server..."
       self.controlChannel.connect()
 
-   # Spawn threads
-   def createAndStartThreads(self):
-      dataThread = myThread(1, "Data Thread")
+   # Create and start additional threads
+   def spawnThreads(self):
+      dataThread = myThread(1, "Data Thread", self.dataChannel)
       dataThread.start()
 
-   # Be able to send messages over each thread separately
+   # TODO: Be able to send messages over each thread separately
 
    def closeConnection(self):
       self.controlChannel.closeConnection()
       print "Connection to the control channel has been closed successfully!"
 
 if __name__ == '__main__':
-   controlPort = 11112
-   dataPort = 222
-   adminPort = 333
+   controlPort = 11111
+   dataPort = 11112
+   adminPort = 11113
    mainThread = Threads(controlPort, dataPort, adminPort)
    mainThread.initiateConnection()
-   #thread.createAndStartThreads()
-   mainThread.closeConnection()
+   mainThread.spawnThreads()
+   #mainThread.closeConnection()
 
